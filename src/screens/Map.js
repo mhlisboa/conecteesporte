82e89664fa3d22f7e681e1dfba9c663aa633f02a
KauanSampaio <kauansampaio53@gmail.com>
2023-09-21 11:42:48 -0300

    marker list automation

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -15,13 +15,31 @@ import {
 import MapView from 'react-native-maps';
 import { Marker } from "react-native-maps";
 import { Ionicons } from "@expo/vector-icons";
+const locations = require("../../geolocation.json");
+
+function renderMarkers() {
+  let markerList = [];
+
+  for(let i=1; i<27; i++) {
+    markerList.push(
+      <Marker 
+        coordinate={
+          {
+            latitude: locations[i].latitude,
+            longitude: locations[i].longitude
+          }
+        }
+      />
+    )
+  }
+
+  return markerList;
+}
 
 
 export default function ({ navigation }) {
   const { isDarkmode, setTheme } = useTheme();
   const auth = getAuth();
-
-  const locations = require("../../geolocation.json");
 
   return (
     <Layout>
@@ -37,206 +55,7 @@ export default function ({ navigation }) {
             }
           }  
         >
-          <Marker
-            coordinate={
-              {
-                latitude: locations[1].latitude, 
-                longitude: locations[1].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[2].latitude, 
-                longitude: locations[2].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[3].latitude, 
-                longitude: locations[3].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[4].latitude, 
-                longitude: locations[4].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[5].latitude, 
-                longitude: locations[5].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[6].latitude, 
-                longitude: locations[6].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[7].latitude, 
-                longitude: locations[7].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[8].latitude, 
-                longitude: locations[8].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[9].latitude, 
-                longitude: locations[9].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[10].latitude, 
-                longitude: locations[10].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[11].latitude, 
-                longitude: locations[11].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[12].latitude, 
-                longitude: locations[12].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[13].latitude, 
-                longitude: locations[13].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[14].latitude, 
-                longitude: locations[14].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[15].latitude, 
-                longitude: locations[15].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[16].latitude, 
-                longitude: locations[16].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[17].latitude, 
-                longitude: locations[17].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[18].latitude, 
-                longitude: locations[18].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[19].latitude, 
-                longitude: locations[19].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[20].latitude, 
-                longitude: locations[20].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[21].latitude, 
-                longitude: locations[21].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[22].latitude, 
-                longitude: locations[22].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[23].latitude, 
-                longitude: locations[23].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[24].latitude, 
-                longitude: locations[24].longitude
-              }
-            }
-          />
-          <Marker
-            coordinate={
-              {
-                latitude: locations[25].latitude, 
-                longitude: locations[25].longitude
-              }
-            }
-          />
+          {renderMarkers()}
         </MapView>
       </View>
       <View style={styles.bottomBar}>
